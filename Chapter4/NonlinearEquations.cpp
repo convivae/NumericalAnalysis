8742bc5ef633a4cd311bf42515964de5e522bac3
convivae <l16271012@gmail.com>
2020-05-01 17:04:06 +0800

    非线性方程的迭代解法完成

--- Chapter4/NonlinearEquations.cpp
+++ Chapter4/NonlinearEquations.cpp
@@ -4,50 +4,221 @@
 
 #include "NonlinearEquations.h"
 
-void convivae::NonlinearEquations::draw_dividing_line()
-{
-	std::cout << "------------------------------------------------" << std::endl;
+void convivae::NonlinearEquations::draw_dividing_line() {
+    std::cout << "------------------------------------------------" << std::endl;
 }
 
-void convivae::NonlinearEquations::Dichotomy(const pFun fun, double left, double right, double epsilon,
-                                             bool show_details, int max_steps)
-{
-	auto k = 0;
-	auto mid = 0.0;
-	while (k++ < max_steps) {
-		mid = (left + right) / 2;
+void convivae::NonlinearEquations::dichotomy_method(const pFun fun, double left, double right, double epsilon,
+                                                    bool show_details, int max_steps) const {
+    std::cout << "二分法" << std::endl;
+    auto k = 0;
+    auto mid = 0.0;
+    while (k++ < max_steps) {
+        mid = (left + right) / 2;
 
-		if (show_details)
-			std::cout << "第" << k << "次迭代:\nmid = " << mid;
+        if (right - left < epsilon) {
+            if (show_details)
+                std::cout << "right - left = " << right - left << " < " << epsilon << std::endl;
+            break;
+        }
 
-		if (right - left < epsilon) {
-			if (show_details)
-				std::cout << ", right - left < " << epsilon << std::endl;
-			break;
-		}
+        if (show_details)
+            std::cout << "第" << k << "次迭代:\nmid = " << mid;
+
+        auto a = fun(left), b = fun(right), c = fun(mid);
+
+        if (fabs(c) < 1e-10) {
+            if (show_details)
+                std::cout << "fabs(f(mid)) = " << fabs(c) << " < 1e-10" << std::endl;
+            break;
+        }
+
+        if (show_details) {
+            std::cout << ", f(left) = " << a << ", f(mid) = " << c << ", f(left) * f(mid)";
+            a * c > 0 ? std::cout << " > 0" << std::endl : std::cout << " < 0" << std::endl;
+        }
 
 
-		auto a = fun(left), b = fun(right), c = fun(mid);
+        if (a * c < 0)
+            right = mid;
+        else
+            left = mid;
 
-		if (fabs(c) < 1e-10) {
-			if (show_details)
-				std::cout << "fabs(f(mid)) = " << fabs(c) << " < 1e-10" << std::endl;
-			break;
-		}
+        if (show_details)
+            std::cout << "在 (" << left << ", " << right << ") 之间" << std::endl;
+    }
 
-		if (show_details)
-			std::cout << ", a * c = " << a * c << std::endl;
+    std::cout << "迭代次数: " << k - 1 << std::endl;
+    std::cout << "结果: x = " << mid << std::endl;
+    std::cout << "验算: fun(x) = " << fun(mid) << std::endl;
+    draw_dividing_line();
+}
 
-		if (a * c < 0)
-			right = mid;
-		else
-			left = mid;
+void
+convivae::NonlinearEquations::simple_iteration_method(convivae::NonlinearEquations::pFun fun, double x0, double eta,
+                                                      bool show_details, int max_steps) const {
+    std::cout << "简单迭代法" << std::endl;
+    auto k = 0;
+    auto xk = x0;
 
-		if (show_details)
-			std::cout << "left = " << left << ", right = " << right << std::endl;
-	}
+    if (show_details)
+        std::cout << "x0 = " << x0 << std::endl;
 
-	std::cout << "迭代次数: " << k << std::endl;
-	std::cout << "结果: " << mid << std::endl;
-	draw_dividing_line();
+    while (k++ < max_steps) {
+        xk = fun(x0);
+
+        if (show_details)
+            std::cout << "x" << k << " = " << xk << std::endl;
+
+        if (fabs(xk - x0) / fabs(xk) <= eta) {
+            if (show_details)
+                std::cout << "|x" << k << " - x" << k - 1 << "| / |x" << k << "| = " << fabs(xk - x0) / fabs(xk)
+                          << " < " << eta << std::endl;
+            break;
+        }
+
+        x0 = xk;
+    }
+
+    std::cout << "迭代次数: " << k << std::endl;
+    std::cout << "结果: x = " << xk << std::endl;
+
+    draw_dividing_line();
+}
+
+void
+convivae::NonlinearEquations::Steffensen_iteration_method(convivae::NonlinearEquations::pFun fun, double x0, double eta,
+                                                          bool show_details, int max_steps) const {
+    std::cout << "Steffensen 迭代法" << std::endl;
+    auto k = 0;
+    auto xk = x0;
+
+    if (show_details)
+        std::cout << "x0 = " << x0 << std::endl;
+
+    while (k++ < max_steps) {
+        auto y = fun(x0), z = fun(y);
+        xk = x0 - (y - x0) * (y - x0) / (z - 2 * y + x0);
+
+        if (show_details)
+            std::cout << "x" << k << " = " << xk << std::endl;
+
+        if (fabs(xk - x0) / fabs(xk) <= eta) {
+            if (show_details)
+                std::cout << "|x" << k << " - x" << k - 1 << "| / |x" << k << "| = " << fabs(xk - x0) / fabs(xk)
+                          << " < " << eta << std::endl;
+            break;
+        }
+
+        x0 = xk;
+    }
+
+    std::cout << "迭代次数: " << k << std::endl;
+    std::cout << "结果: x = " << xk << std::endl;
+    draw_dividing_line();
+}
+
+void convivae::NonlinearEquations::Newton_iteration_method(convivae::NonlinearEquations::pFun fun,
+                                                           convivae::NonlinearEquations::pFun fun_1, double eta,
+                                                           double x0, bool show_details, bool down_hill,
+                                                           int max_steps) const {
+    // 下山法确定初始值
+    if (down_hill) {
+        auto lambda = 1.0;
+        auto y = x0 - lambda * fun(x0) / fun_1(x0);
+        while (fabs(fun(y)) > fabs(fun(x0))) {
+            lambda /= 2;
+            y = x0 - lambda * fun(x0) / fun_1(x0);
+        }
+        x0 = y;
+    }
+
+    std::cout << "Newton 迭代法" << std::endl;
+    auto k = 0;
+    auto xk = x0;
+
+    if (show_details)
+        std::cout << "x0 = " << x0 << std::endl;
+
+    while (k++ < max_steps) {
+        xk = x0 - fun(x0) / fun_1(x0);
+
+        if (show_details)
+            std::cout << "x" << k << " = " << xk << std::endl;
+
+        if (fabs(xk - x0) / fabs(xk) <= eta) {
+            if (show_details)
+                std::cout << "|x" << k << " - x" << k - 1 << "| / |x" << k << "| = " << fabs(xk - x0) / fabs(xk)
+                          << " < " << eta << std::endl;
+            break;
+        }
+
+        x0 = xk;
+    }
+
+    std::cout << "迭代次数: " << k << std::endl;
+    std::cout << "结果: x = " << xk << std::endl;
+    draw_dividing_line();
+}
+
+void convivae::NonlinearEquations::secant_iteration_method(convivae::NonlinearEquations::pFun fun, double x0, double x1,
+                                                           double eta, bool show_details, int max_steps) const {
+    std::cout << "割线法" << std::endl;
+    auto k = 1;
+    auto xk = x0;
+
+    if (show_details)
+        std::cout << "x0 = " << x0 << ", x1 = " << x1 << std::endl;
+
+    while (k++ < max_steps) {
+        xk = x1 - (fun(x1) * (x1 - x0)) / (fun(x1) - fun(x0));
+
+        if (show_details)
+            std::cout << "x" << k << " = " << xk << std::endl;
+
+        if (fabs(xk - x1) / fabs(xk) <= eta) {
+            if (show_details)
+                std::cout << "|x" << k << " - x" << k - 1 << "| / |x" << k << "| = " << fabs(xk - x1) / fabs(xk)
+                          << " < " << eta << std::endl;
+            break;
+        }
+
+        x0 = x1;
+        x1 = xk;
+    }
+
+    std::cout << "迭代次数: " << k - 1 << std::endl;
+    std::cout << "结果: x = " << xk << std::endl;
+    draw_dividing_line();
+}
+
+void convivae::NonlinearEquations::secant_with_single_point_iteration_method(convivae::NonlinearEquations::pFun fun,
+                                                                             double x0, double x1, double eta,
+                                                                             bool show_details, int max_steps) const {
+    std::cout << "单点割线法" << std::endl;
+    auto k = 1;
+    auto xk = x0, f_x0 = fun(x0);
+
+    if (show_details)
+        std::cout << "x0 = " << x0 << ", x1 = " << x1 << std::endl;
+
+    while (k++ < max_steps) {
+        xk = x1 - (fun(x1) * (x1 - x0)) / (fun(x1) - f_x0);
+
+        if (show_details)
+            std::cout << "x" << k << " = " << xk << std::endl;
+
+        if (fabs(xk - x1) / fabs(xk) <= eta) {
+            if (show_details)
+                std::cout << "|x" << k << " - x" << k - 1 << "| / |x" << k << "| = " << fabs(xk - x1) / fabs(xk)
+                          << " < " << eta << std::endl;
+            break;
+        }
+
+        x1 = xk;
+    }
+
+    std::cout << "迭代次数: " << k - 1 << std::endl;
+    std::cout << "结果: x = " << xk << std::endl;
+    draw_dividing_line();
 }
--- Chapter4/NonlinearEquations.h
+++ Chapter4/NonlinearEquations.h
@@ -10,36 +10,94 @@
 #include "../Type/Type.h"
 #include <iostream>
 #include <vector>
+#include <cmath>
+#include <iomanip>
 
 namespace convivae {
-	class NonlinearEquations {
-	private:
-		//定义一种 pFun 的的函数指针，这种函数以一个 double 为参数并返回 double 类型
-		typedef f8 (*pFun)(f8 x);
+    class NonlinearEquations {
+    private:
+        //定义一种 pFun 的的函数指针，这种函数以一个 double 为参数并返回 double 类型
+        typedef f8 (*pFun)(f8 x);
 
-		// 函数指针，分别指向迭代函数、迭代函数的一阶导数
-		//pFun fun_, fun_1_;
+        static void draw_dividing_line();
 
-		static void draw_dividing_line();
+    public:
+        /**
+         * 二分法（对分法）
+         * 只能求单根和奇数重根
+         * @param fun 迭代函数的函数指针
+         * @param left 左区间
+         * @param right 右区间
+         * @param epsilon 迭代精度
+         * @param max_steps 最大迭代次数
+         */
+        void dichotomy_method(pFun fun, double left, double right, double epsilon = 1e-6, bool show_details = false,
+                              int max_steps = 2000) const;
 
-	public:
-		// NonlinearEquations(const pFun fun, const pFun fun_1)
-		//  : fun_(fun),fun_1_(fun_1){}
-
-		/**
-		 * 二分法（对分法）
-		 * 只能求单根和奇数重根
-		 * @param fun 迭代函数的函数指针
-		 * @param left 左区间
-		 * @param right 右区间
-		 * @param epsilon 迭代精度
-		 * @param max_steps 最大迭代次数
-		 */
-		void Dichotomy(const pFun fun, double left, double right, double epsilon = 1e-5, bool show_details = false,
-		               int max_steps = 2000);
+        /**
+         * 简单迭代法
+         * @param fun 函数指针
+         * @param x0 迭代初始值
+         * @param eta 迭代精度
+         * @param show_details
+         * @param max_steps
+         */
+        void simple_iteration_method(pFun fun, double x0, double eta = 1e-6, bool show_details = false,
+                                     int max_steps = 2000) const;
 
 
-	};
+        /**
+         * Steffensen 迭代法
+         * @param fun 函数指针
+         * @param x0 迭代初始值
+         * @param eta 迭代精度
+         * @param show_details
+         * @param max_steps
+         */
+        void Steffensen_iteration_method(pFun fun, double x0, double eta = 1e-6, bool show_details = false,
+                                         int max_steps = 2000) const;
+
+
+        /**
+         * Newton 迭代法
+         * 使用了牛顿下山法来解决初始值选定后不收敛的问题，因此无需给定初始值
+         * @param fun 迭代函数
+         * @param fun_1 迭代函数的一阶导数
+         * @param x0
+         * @param eta
+         * @param show_details
+         * @param down_hill 是否采用下山法，若选定的初始值不收敛可以选择开启
+         * @param max_steps
+         */
+        void Newton_iteration_method(pFun fun, pFun fun_1, double eta = 1e-6, double x0 = 0, bool show_details = false,
+                                     bool down_hill = false, int max_steps = 2000) const;
+
+        /**
+         * 割线法
+         * 把 Newton 法中的导数替换为增量比，收敛速度低于 Newton 法，但高于一阶
+         * @param fun
+         * @param x0 两个初始值
+         * @param x1 两个初始值
+         * @param eta
+         * @param show_details
+         * @param max_steps
+         */
+        void secant_iteration_method(pFun fun, double x0, double x1, double eta = 1e-6, bool show_details = false,
+                                     int max_steps = 2000) const;
+
+        /**
+         * 单点割线法
+         * 在割线法中用固定点 (x0, f(x0)) 代替 (x_(k-1), f(x_(k-1)))，即 (x0, f(x0)) 永远是割线上的一点
+         * @param fun
+         * @param x0 两个初始值
+         * @param x1 两个初始值
+         * @param eta
+         * @param show_details
+         * @param max_steps
+         */
+        void secant_with_single_point_iteration_method(pFun fun, double x0, double x1, double eta = 1e-6, bool show_details = false,
+                                     int max_steps = 2000) const;
+    };
 }
 
 #endif //NUMERICALANALYSIS_NONLINEAREQUATIONS_H
